a2b3e7e7c7f5db1ac7c04ffb2faaa5e0b91ec429
Vladyslav <chonkavladd@gmail.com>
2025-07-05 20:31:46 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
+
         scaffoldBackgroundColor: const Color(0xff121212),
+
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(
           backgroundColor: Color(0xff121212),
           selectedItemColor: Colors.white,
           unselectedItemColor: Colors.grey,
+          elevation: 0,
         ),
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+        splashFactory: NoSplash.splashFactory,
+
       ),
       debugShowCheckedModeBanner: false,
       home: const MainPage(),
@@ -57,34 +64,37 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff121212),
+
       appBar: AppBar(
         title: const Text('Famble', style: TextStyle(fontSize: 25, fontFamily: 'Arial')),
         centerTitle: true,
         backgroundColor: const Color(0xff121212),
       ),
+
       body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.checklist),
-            label: 'Challenges',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.analytics),
-            label: 'Statistic',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings',
-          ),
-        ],
+
+      bottomNavigationBar: Container(
+        color: const Color(0xff121212),
+
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          currentIndex: _selectedIndex,
+          onTap: _onTap,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          type: BottomNavigationBarType.fixed,
+
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.checklist), label: 'Challenges'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.analytics), label: 'Statistic'),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
+          ],
+        ),
       ),
+
     );
   }
 }
